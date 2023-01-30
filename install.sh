47218a9100565167d413abf70e43981e9e1045f5
Anagnostakis Ioannis <412135+rizitis@users.noreply.github.com>
2023-01-30 20:19:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,12 +15,12 @@ PWD=$(pwd)
 
 set -e
 
-# These files MUST be placed always upstream to be always updated, thats sexyslack job when it running not now.
-rm BLACKLIST*
-
 # The default blacklist sexyslack doing to Gnome repo to save space and time...
 mkdir -p /etc/sexyslack/ || exit
 cp blacklist.txt /etc/sexyslack/
-
+# These files MUST be placed always upstream, when sexyslack run first time will replace them, but needed to exist there for now.
+mkdir -p /usr/src/sexyslack/Gnome || exit
+cp BLACKLIST* /usr/src/sexyslack/Gnome/
+# make script executable and put it to $PATH
 chmod +x sexyslack && cp sexyslack /usr/local/bin/
 chmod +x sexyslack-uninstall && cp sexyslack-uninstall /usr/local/bin/
